c305a30291fcc69b51ac6c98cab2c96397040d28
willgertsch <willgertsch+github@gmail.com>
2023-12-18 12:26:11 -0800

    started simulation 2 code

--- simulation.R
+++ simulation.R
@@ -30,6 +30,7 @@ algorithms = c(
 # try for all example data
 # n=10 samples
 # median ll
+# default is pop=50, maxIter=500
 
 # investigate example data
 data("carcinoma") # binary ratings of 7 pathologists for 118 slides
@@ -46,7 +47,6 @@ head(values)
 
 # test algorithms on carcinoma data
 # nsim: number of samples per algorithm
-# default is pop=50, maxIter=500
 sim1_carcinoma = function(nsim, algorithms, seed) {
   
   # based on ?carcinoma example
@@ -75,22 +75,334 @@ sim1_carcinoma = function(nsim, algorithms, seed) {
 
 sim1_carcinoma_result = sim1_carcinoma(10, algorithms, 1234)
 sim1_carcinoma_result
-apply(sim1_carcinoma_result, 2, median, na.rm = T)
+apply(sim1_carcinoma_result, 2, median, na.rm = T) %>% sort() # DE, GWO, HS
+which.max(apply(sim1_carcinoma_result, 2, median, na.rm = T))
 
 # test algorithms on cheating data
+sim1_cheating = function(nsim, algorithms, seed) {
+  
+  f <- cbind(LIEEXAM,LIEPAPER,FRAUD,COPYEXAM)~1
+  results = matrix(data = NA, nrow = nsim, ncol = length(algorithms))
+  
+  for (alg in 1:length(algorithms)) {
+    cat(paste0("Running ", algorithms[alg], '\n'))
+    for (sim in 1:nsim) {
+      
+      try(
+        results[sim, alg] <- poLCA(f,cheating,nclass=2,meta_control = list(
+          method = 'metaheuristic',
+          algorithm = algorithms[alg],
+          seed = seed+sim # starting places are the same across algorithms
+        ))$llik, TRUE)
+      
+      
+    }
+  }
+  results = as.data.frame(results)
+  colnames(results) = algorithms
+  results
+}
+
+sim1_cheating_result = sim1_cheating(10, algorithms, 1234)
+sim1_cheating_result
+apply(sim1_cheating_result, 2, median, na.rm = T)  %>% sort() # PSO, MFO, WOA
+which.max(apply(sim1_cheating_result, 2, median, na.rm = T))
+
 # test algorithms on election data
+sim1_election = function(nsim, algorithms, seed) {
+  
+  f <- cbind(MORALG,CARESG,KNOWG,LEADG,DISHONG,INTELG,
+             MORALB,CARESB,KNOWB,LEADB,DISHONB,INTELB)~1
+  results = matrix(data = NA, nrow = nsim, ncol = length(algorithms))
+  
+  for (alg in 1:length(algorithms)) {
+    cat(paste0("Running ", algorithms[alg], '\n'))
+    for (sim in 1:nsim) {
+      
+      try(
+        results[sim, alg] <- poLCA(f,election,nclass=3,meta_control = list(
+          method = 'metaheuristic',
+          algorithm = algorithms[alg],
+          seed = seed+sim # starting places are the same across algorithms
+        ))$llik, TRUE)
+      
+      
+    }
+  }
+  results = as.data.frame(results)
+  colnames(results) = algorithms
+  results
+}
+
+sim1_election_result = sim1_election(10, algorithms, 1234)
+sim1_election_result
+apply(sim1_election_result, 2, median, na.rm = T) %>% sort() # WOA, GWO, HS
+which.max(apply(sim1_election_result, 2, median, na.rm = T))
+
 # test algorithms on gss82 data
+sim1_gss82 = function(nsim, algorithms, seed) {
+  
+  f <- cbind(PURPOSE,ACCURACY,UNDERSTA,COOPERAT)~1
+  results = matrix(data = NA, nrow = nsim, ncol = length(algorithms))
+  
+  for (alg in 1:length(algorithms)) {
+    cat(paste0("Running ", algorithms[alg], '\n'))
+    for (sim in 1:nsim) {
+      
+      try(
+        results[sim, alg] <- poLCA(f,gss82,nclass=4,meta_control = list(
+          method = 'metaheuristic',
+          algorithm = algorithms[alg],
+          seed = seed+sim # starting places are the same across algorithms
+        ))$llik, TRUE)
+      
+      
+    }
+  }
+  results = as.data.frame(results)
+  colnames(results) = algorithms
+  results
+}
+
+sim1_gss82_result = sim1_gss82(10, algorithms, 1234)
+sim1_gss82_result
+apply(sim1_gss82_result, 2, median, na.rm = T) %>% sort() # DE, GWO, HS
+which.max(apply(sim1_gss82_result, 2, median, na.rm = T))
+
 # test algorithms on values data
+sim1_values = function(nsim, algorithms, seed) {
+  
+  f <- cbind(A,B,C,D)~1
+  results = matrix(data = NA, nrow = nsim, ncol = length(algorithms))
+  
+  for (alg in 1:length(algorithms)) {
+    cat(paste0("Running ", algorithms[alg], '\n'))
+    for (sim in 1:nsim) {
+      
+      try(
+        results[sim, alg] <- poLCA(f,values,nclass=3,meta_control = list(
+          method = 'metaheuristic',
+          algorithm = algorithms[alg],
+          seed = seed+sim # starting places are the same across algorithms
+        ))$llik, TRUE)
+      
+      
+    }
+  }
+  results = as.data.frame(results)
+  colnames(results) = algorithms
+  results
+}
+
+sim1_values_result = sim1_values(10, algorithms, 1234)
+sim1_values_result
+apply(sim1_values_result, 2, median, na.rm = T) %>% sort() # WOA, DE, ALO
+which.max(apply(sim1_values_result, 2, median, na.rm = T))
 
 # table of results
-# plot of raw values
-# take top 3 algorithms to next stage
+library(dplyr)
+sim1_combined_results = rbind(sim1_carcinoma_result, sim1_cheating_result,
+      sim1_election_result, sim1_gss82_result, sim1_values_result) %>%
+  as.data.frame() %>%
+  mutate(dataset = rep(c("carcinoma", 'cheating', 'election', 'gss82',
+                         'values'), each = 10))
 
+sim1_combined_results
+write.csv(sim1_combined_results, 'sim1_combined_results.csv')
+
+sim1_combined_results_summarised = rbind(
+  apply(sim1_carcinoma_result, 2, median, na.rm = T),
+  apply(sim1_cheating_result, 2, median, na.rm = T),
+  apply(sim1_election_result, 2, median, na.rm = T),
+  apply(sim1_gss82_result, 2, median, na.rm = T),
+  apply(sim1_values_result, 2, median, na.rm = T)
+) %>%
+  as.data.frame() %>%
+  mutate(dataset = rep(c("carcinoma", 'cheating', 'election', 'gss82',
+                         'values'), each = 1))
+sim1_combined_results_summarised
+write.csv(sim1_combined_results_summarised, 'sim1_combined_results_summarised.csv')
+
+# which have the highest medians on each dataset?
+
+# plot of raw values
+library(tidyr)
+sim1_combined_results %>%
+  pivot_longer(cols = PSO:BHO, names_to = 'algorithm', values_to = 'loglikelihood') %>%
+  filter(!is.na(loglikelihood)) %>%
+  ggplot(aes(x =algorithm, y = loglikelihood)) +
+  geom_boxplot() +
+  facet_wrap(~dataset, scales = 'free')+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+# filter to better algorithms
+# all algorithms were top 3 at some problem
+sim1_combined_results %>%
+  pivot_longer(cols = PSO:BHO, names_to = 'algorithm', values_to = 'loglikelihood') %>%
+  filter(!is.na(loglikelihood)) %>%
+  filter(algorithm %in% c('DE', 'GWO', 'HS', 'ALO', 'PSO', 'MFO', 'WOA')) %>%
+  ggplot(aes(x =algorithm, y = loglikelihood)) +
+  geom_point() +
+  facet_wrap(~dataset, scales = 'free')+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
+# take top algorithms to next stage
+top_algorithms = c('DE', 'GWO', 'HS', 'WOA')
+sim1_combined_results %>%
+  pivot_longer(cols = PSO:BHO, names_to = 'algorithm', values_to = 'loglikelihood') %>%
+  filter(!is.na(loglikelihood)) %>%
+  filter(algorithm %in% top_algorithms) %>%
+  ggplot(aes(x =algorithm, y = loglikelihood)) +
+  geom_point() +
+  facet_wrap(~dataset, scales = 'free')+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 # simulation 2:
 # determine optimal values for population and iterations
 # factorial experiment + fit regression model
-# for top 3 algorithms only
+# for top 4 algorithms only
+iter = c(100, 250, 500, 750, 1000, 2000)
+swarm = c(10, 20, 50, 75, 100, 200)
+algorithms = c('DE', 'GWO', 'HS', 'WOA')
+
+# nsim: number of simulations
+# algorithms: selected algorithms
+# iter: number of iterations
+# swarm: size the swarm, always smaller than iter
+
+sim2_carcinoma = function(nsim, algorithms, iter, swarm, seed) {
+  
+  # set up model
+  f <- cbind(A,B,C,D,E,F,G)~1
+  
+  # set up results data structure
+  results = expand.grid(iter, swarm, algorithms)
+  results$loglikelihood = rep(NA, nrow(results))
+  colnames(results) = c('iter', 'swarm', 'algorithm', 'loglikelihood')
+  
+  # iterate through algorithms, iter, swarm
+  for (i in 1:nrow(results)) {
+    cat('Progress: ', i, '/', nrow(results), '\n')
+    cat('Alg: ', as.character(results[i, 3]), ' iter: ', results[i, 1], ' swarm: ', results[i,2], '\n')
+    results[i, 4] <- poLCA(f,carcinoma,nclass=4,meta_control = list(
+      method = 'metaheuristic',
+      algorithm = results[i, 3],
+      iter = results[i, 1],
+      swarm = results[i, 2],
+      seed = seed+i # starting places are the same across algorithms
+    ),verbose=F)$llik
+  }
+  
+  return(results)
+  
+}
+
+sim2_carcinoma_results = sim2_carcinoma(10, algorithms, iter, swarm,1234)
+sim2_carcinoma_results
+write.csv(sim2_carcinoma_results, 'sim2_carcinoma_results.csv')
+
+sim2_cheating = function(nsim, algorithms, iter, swarm) {
+  # set up model
+  f <- cbind(LIEEXAM,LIEPAPER,FRAUD,COPYEXAM)~1
+  
+  # set up results data structure
+  results = expand.grid(iter, swarm, algorithms)
+  results$loglikelihood = rep(NA, nrow(results))
+  colnames(results) = c('iter', 'swarm', 'algorithm', 'loglikelihood')
+  
+  # iterate through algorithms, iter, swarm
+  for (i in 1:nrow(results)) {
+    cat('Progress: ', i, '/', nrow(results), '\n')
+    cat('Alg: ', as.character(results[i, 3]), ' iter: ', results[i, 1], ' swarm: ', results[i,2], '\n')
+    results[i, 4] <- poLCA(f,cheating,nclass=2,meta_control = list(
+      method = 'metaheuristic',
+      algorithm = results[i, 3],
+      iter = results[i, 1],
+      swarm = results[i, 2],
+      seed = seed+i # starting places are the same across algorithms
+    ))$llik
+  }
+  
+  return(results)
+}
+
+sim2_election = function(nsim, algorithms, iter, swarm) {
+  # set up model
+  f <- cbind(MORALG,CARESG,KNOWG,LEADG,DISHONG,INTELG,
+             MORALB,CARESB,KNOWB,LEADB,DISHONB,INTELB)~1
+  
+  # set up results data structure
+  results = expand.grid(iter, swarm, algorithms)
+  results$loglikelihood = rep(NA, nrow(results))
+  colnames(results) = c('iter', 'swarm', 'algorithm', 'loglikelihood')
+  
+  # iterate through algorithms, iter, swarm
+  for (i in 1:nrow(results)) {
+    cat('Progress: ', i, '/', nrow(results), '\n')
+    cat('Alg: ', as.character(results[i, 3]), ' iter: ', results[i, 1], ' swarm: ', results[i,2], '\n')
+    results[i, 4] <- poLCA(f,election,nclass=3,meta_control = list(
+      method = 'metaheuristic',
+      algorithm = results[i, 3],
+      iter = results[i, 1],
+      swarm = results[i, 2],
+      seed = seed+i # starting places are the same across algorithms
+    ))$llik
+  }
+  
+  return(results)
+}
+
+sim2_gss82 = function(nsim, algorithms, iter, swarm) {
+  # set up model
+  f <- cbind(PURPOSE,ACCURACY,UNDERSTA,COOPERAT)~1
+  
+  # set up results data structure
+  results = expand.grid(iter, swarm, algorithms)
+  results$loglikelihood = rep(NA, nrow(results))
+  colnames(results) = c('iter', 'swarm', 'algorithm', 'loglikelihood')
+  
+  # iterate through algorithms, iter, swarm
+  for (i in 1:nrow(results)) {
+    cat('Progress: ', i, '/', nrow(results), '\n')
+    cat('Alg: ', as.character(results[i, 3]), ' iter: ', results[i, 1], ' swarm: ', results[i,2], '\n')
+    results[i, 4] <- poLCA(f,gss82,nclass=4,meta_control = list(
+      method = 'metaheuristic',
+      algorithm = results[i, 3],
+      iter = results[i, 1],
+      swarm = results[i, 2],
+      seed = seed+i # starting places are the same across algorithms
+    ))$llik
+  }
+  
+  return(results)
+}
+
+sim2_values = function(nsim, algorithms, iter, swarm) {
+  # set up model
+  f <- cbind(A,B,C,D)~1
+  
+  # set up results data structure
+  results = expand.grid(iter, swarm, algorithms)
+  results$loglikelihood = rep(NA, nrow(results))
+  colnames(results) = c('iter', 'swarm', 'algorithm', 'loglikelihood')
+  
+  # iterate through algorithms, iter, swarm
+  for (i in 1:nrow(results)) {
+    cat('Progress: ', i, '/', nrow(results), '\n')
+    cat('Alg: ', as.character(results[i, 3]), ' iter: ', results[i, 1], ' swarm: ', results[i,2], '\n')
+    results[i, 4] <- poLCA(f,values,nclass=3,meta_control = list(
+      method = 'metaheuristic',
+      algorithm = results[i, 3],
+      iter = results[i, 1],
+      swarm = results[i, 2],
+      seed = seed+i # starting places are the same across algorithms
+    ))$llik
+  }
+  
+  return(results)
+}
+
 
 # simulation 3:
 # compare top 3 metaheuristics, EM algorithm, hybrid EM with best metaheuristic
